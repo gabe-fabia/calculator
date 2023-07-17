9353781605c9a0f5cd00c51bdbc40f88091d3a56
Gabe Fabia <52291257+gabe-fabia@users.noreply.github.com>
2023-07-17 15:25:01 -0400

    added boilerplate html and simple math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+};
+console.log(add(2,3));
+
+function subtract(a, b) {
+    return a - b;
+};
+console.log(subtract(5,2));
+
+function multiply(a, b) {
+    return a * b;
+};
+console.log(multiply(9,9));
+
+function divide(a, b) {
+    return a / b;
+};
+console.log(divide(12,3));
+console.log(divide(12,0));
